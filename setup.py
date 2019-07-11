7d8a62c2c096c04b53e53d7c8a045f29f2207645
Eric Gazoni <eric@adimian.com>
2019-07-11 14:49:26 +0200

    render readme as markdown

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
 
-
-setup(setup_requires=["pbr"], pbr=True)
+setup(
+    setup_requires=[
+        "pbr>=5.4.0",
+        "setuptools>=38.6.0",
+        "twine>=1.11.0",
+        "wheel>=0.31.0",
+    ],
+    pbr=True,
+)
